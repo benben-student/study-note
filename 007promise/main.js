b4809f51f02c45f44d89c176106a0fc4cd038a7e
benben-student <a441832620@126.com>
2022-06-28 20:33:35 +0800

    promise

--- 007promise/main.js
+++ 007promise/main.js
@@ -26,3 +26,26 @@
 //   .then(value=>{
 //       console.log(value+'world')
 //   })
+//(3)
+console.log('开始')
+new Promise(resolve => {
+    setTimeout(() => {
+        resolve('hello')
+    }, 2000)
+})
+    .then(value => {
+        console.log(value)
+        console.log('everyone')
+            (function () {
+                return new Promise(resolve => {
+                    setTimeout(() => {
+                        console.log('ML')
+                        resolve('Ml')
+                    }, 2000)
+                })
+            }())
+        return false
+    })
+    .then(value => {
+        console.log(value + 'world')
+    })
