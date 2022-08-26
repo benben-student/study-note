7832a35d84aebbe4b96c5e3be551b09808e3d7aa
benben-student <a441832620@126.com>
2022-08-26 10:22:22 +0800

    doc : configuration file

---
+++ 012TypeScript/Config/config.ts
@@ -0,0 +1,25 @@
+//编译 tsc xxx.xxx
+//编译后不加注释 直接 tsc
+
+const person: string = "js"
+
+// "noImplicitAny": true,
+//允许你的注解类型any，不用特意标明
+
+
+// "strictNullChecks": true,
+//允许null值为true，可以出现null值
+
+
+//编译文件的存放路径
+// "rootDir": "./(config.json)",  
+
+//编译后的文件的存放路径
+// "outDir": "./build",
+
+//生成编译前后的文件的路径和信息
+// "sourceMap": true,  
+
+//变量未使用编译时会提醒
+// "noUnusedLocals": true, 
+
--- 012TypeScript/Demo14.ts
+++ 012TypeScript/Demo14.ts
@@ -1,11 +1,39 @@
-class Person {
-    //readonly的作用  外部的不能修改数据
-    public readonly _name: string
-    constructor(name: string) {
-        this._name = name
+//补坑
+// class Person {
+//     //readonly的作用  外部的不能修改数据
+//     public readonly _name: string
+//     constructor(name: string) {
+//         this._name = name
+//     }
+// }
+
+// const person = new Person("js")
+
+// console.log(person._name);
+
+//抽象类
+
+abstract class Girl {
+    abstract skill()
+}
+
+class Waiter extends Girl {
+    //子类必须有父类的方法
+    skill() {
+        console.log("1");
     }
 }
 
-const person = new Person("js")
+class BaseTeacher extends Girl {
+    //子类必须有父类的方法
+    skill() {
+        console.log("2");
+    }
+}
 
-console.log(person._name);
+class seniorTeacher extends Girl {
+    //子类必须有父类的方法
+    skill() {
+        console.log("3");
+    }
+}
